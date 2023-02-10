6f43a35411edf794e6e2f817f48a022b4ce57ade
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-10 00:06:34 -0600

    :construction: WIP: Adds basic home layout

--- feria_solidaridad/lib/modules/home/view/home.dart
+++ feria_solidaridad/lib/modules/home/view/home.dart
@@ -1,14 +1,56 @@
+import 'package:feria_solidaridad/constants/theme_constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home"),
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: kPrimaryColor,
+            centerTitle: true,
+            pinned: true,
+            floating: true,
+            expandedHeight: 160.0,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                "Feria de la Solidaridad",
+                style: Theme.of(context).textTheme.headline6?.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+              background: const FlutterLogo(),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "XXI Feria de la Solidaridad",
+                    style: Theme.of(context).textTheme.overline?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  Text(
+                    "¡Bienvenido!",
+                    style: Theme.of(context).textTheme.headline5?.copyWith(
+                          color: kSecondaryColor,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
