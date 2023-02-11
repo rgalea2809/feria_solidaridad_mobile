baa4cc108fdbd05c39240f7d11b21b18871c9631
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-11 16:20:40 -0600

    :fire: Removes unnecessary widget

--- feria_solidaridad/lib/modules/home/view/home.dart
+++ feria_solidaridad/lib/modules/home/view/home.dart
@@ -52,20 +52,6 @@ class Home extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  color: Colors.transparent,
-                  height: 100.0,
-                  child: Center(
-                    child: Text('$index', textScaleFactor: 5),
-                  ),
-                );
-              },
-              childCount: 20,
-            ),
-          ),
         ],
       ),
     );
