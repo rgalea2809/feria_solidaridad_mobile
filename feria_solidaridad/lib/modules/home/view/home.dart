f46c9bde4c5ddf47ed9623a7bc9dabb03579f0ed
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-11 17:01:02 -0600

    :lipstick: Adds welcome section of home screen

--- feria_solidaridad/lib/modules/home/view/home.dart
+++ feria_solidaridad/lib/modules/home/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:feria_solidaridad/constants/debug_constants.dart';
 import 'package:feria_solidaridad/constants/theme_constants.dart';
 import 'package:flutter/material.dart';
 
@@ -10,49 +11,168 @@ class Home extends StatelessWidget {
       backgroundColor: kPrimaryColor,
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            backgroundColor: kPrimaryColor,
-            centerTitle: true,
-            pinned: true,
-            floating: true,
-            expandedHeight: 160.0,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: Text(
-                "Feria de la Solidaridad",
-                style: Theme.of(context).textTheme.headline6?.copyWith(
-                      color: Colors.white,
-                    ),
-              ),
-              background: Image.asset(
-                "assets/images/home_hero.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+          const HomeAppBar(),
+          const HomeWelcomeSection(),
           SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "XXI Feria de la Solidaridad",
-                    style: Theme.of(context).textTheme.overline?.copyWith(
-                          color: Colors.white,
-                        ),
+            child: Stack(
+              children: [
+                Container(
+                  color: kPrimaryColor,
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
                   ),
-                  Text(
-                    "¡Bienvenido!",
-                    style: Theme.of(context).textTheme.headline5?.copyWith(
-                          color: kSecondaryColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 32.0, horizontal: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "Un mensaje del Rector",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6
+                                  ?.copyWith(
+                                    color: kAccentColor,
+                                  ),
+                            ),
+                          ],
                         ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: kPrimaryColor,
+      centerTitle: true,
+      pinned: true,
+      floating: true,
+      expandedHeight: 160.0,
+      leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text(
+          "Feria de la Solidaridad",
+          style: Theme.of(context).textTheme.headline6?.copyWith(
+                color: Colors.white,
+              ),
+        ),
+        background: Image.asset(
+          "assets/images/home_hero.png",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeWelcomeSection extends StatelessWidget {
+  const HomeWelcomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "XXI Feria de la Solidaridad",
+              style: Theme.of(context).textTheme.overline?.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            Text(
+              "¡Bienvenido!",
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    color: kSecondaryColor,
+                  ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Container(
+              constraints: const BoxConstraints(maxHeight: 400),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: kAccentColor,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      // TODO: Change this String to actual paragraph
+                      child: Text(
+                        kLoremParagraph,
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16.0,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: kSecondaryColor,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset("assets/logo/logo-1.png"),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
