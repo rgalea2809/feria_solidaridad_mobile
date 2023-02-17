809f2af2bebcc904ad90bd420f11e0aafcf03b52
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-17 17:42:03 -0600

    :construction: Adds app coordinator

---
+++ feria_solidaridad/lib/modules/app_coordinator/view/app_coordinator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppCoordinator extends StatelessWidget {
+  const AppCoordinator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
