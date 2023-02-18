ef2766dd9925be965900e1fdc5ee55b02d2f2754
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-17 20:55:00 -0600

    :construction: Add institutions tab template

---
+++ feria_solidaridad/lib/modules/institutions/view/institutions_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class InstitutionsTab extends StatelessWidget {
+  const InstitutionsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Instituciones"),
+      ),
+      body: ,
+    );
+  }
+}
