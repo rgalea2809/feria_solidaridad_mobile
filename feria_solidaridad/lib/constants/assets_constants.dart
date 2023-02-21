946c37e41a91582e92475175fa5003ac92a1fbe7
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-20 20:04:13 -0600

    :bento: Adds placeholder image

--- feria_solidaridad/lib/constants/assets_constants.dart
+++ feria_solidaridad/lib/constants/assets_constants.dart
@@ -1,3 +1,5 @@
 // App assets
+const String kDefaultPlaceholderImage =
+    "assets/images/default-placeholder-image.png";
 const String kLogoPath = "assets/images/logo_feria.png";
 const String kHomeHeroPath = "assets/images/home_hero.png";
