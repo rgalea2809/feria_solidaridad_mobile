0d1132e21e4aacc3e68f72ff78aad7e6bcf9fe9b
Rodrigo Mejia Galea <54407065+rgalea2809@users.noreply.github.com>
2023-02-24 16:27:46 -0600

    :sparkles: Creates image loader widget

---
+++ feria_solidaridad/lib/widgets/image_loader.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:feria_solidaridad/constants/assets_constants.dart';
+import 'package:flutter/material.dart';
+
+class ImageLoader extends StatelessWidget {
+  const ImageLoader({super.key, required this.imageUrl});
+  final String? imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      if (imageUrl != null) {
+        return CachedNetworkImage(
+          imageUrl: imageUrl!,
+          placeholder: (context, url) {
+            return const CircularProgressIndicator();
+          },
+          errorWidget: (context, url, error) {
+            return Text("Image $url not found! ($error)");
+          },
+        );
+      }
+
+      return Image.asset(kDefaultPlaceholderImage);
+    });
+  }
+}
